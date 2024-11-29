e1e94d46f03880864f0152428d08a84752986c9c
Tareq <razzbabut@gmail.com>
2024-11-29 21:48:32 +0600

    redesign the login page again

--- src/pages/Authencation/Login.jsx
+++ src/pages/Authencation/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
                 // navigate('/dashboard/userProfile')
                 navigate(location?.state ? location.state : '/dashboard/userProfile')
             })
-            .catch(()=>{
+            .catch(() => {
                 Swal.fire({
                     position: "top-end",
                     icon: "error",
@@ -55,40 +55,42 @@ const Login = () => {
             })
     }
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                </div>
-                <div className="card card-body shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" placeholder="email" className="input input-bordered" {...register("email", { required: true })} />
-                            {errors.email && <span>This field is required</span>}
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" placeholder="password" className="input input-bordered" {...register("password", { required: true })} />
-                            {errors.password && <span>This field is required</span>}
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-                        </div>
-                    </form>
-                    <div className="mt-6">
-                        <button onClick={handleGoogleLogin} className="btn btn-block"><FcGoogle className="text-2xl"></FcGoogle> Google</button>
-                    </div>
-                    <p><small>New here? <Link to='/register'>Create an account</Link></small></p>
-                </div>
+        <div className="w-full max-w-xl p-4 rounded-md shadow sm:p-8 bg-gray-900 text-gray-100 mx-auto">
+            <h2 className="mb-3 text-3xl font-semibold text-center">Login to your account</h2>
+            <p className="text-normal text-center text-gray-400 dark:text-gray-600">Dont have account?
+                <Link to='/register' rel="noopener noreferrer" className="focus:underline hover:underline hover:text-blue-600"> Sign up</Link>
+            </p>
+            <div className="my-6 space-y-4">
+                <button onClick={handleGoogleLogin} aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 border-gray-400 dark:border-gray-600 focus:ring-violet-400 focus:dark:ring-violet-600 hover:border-gray-300">
+                    <FcGoogle className="text-2xl"></FcGoogle>
+                    <p>Login with Google</p>
+                </button>
             </div>
+            <div className="flex items-center w-full my-4">
+                <hr className="w-full text-gray-400 dark:text-gray-600" />
+                <p className="px-3 text-gray-400 dark:text-gray-600">OR</p>
+                <hr className="w-full text-gray-400 dark:text-gray-600" />
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+                <div className="space-y-4 form-control">
+                    <div className="space-y-2 form-control">
+                        <label htmlFor="email" className="block text-sm">Email address</label>
+                        <input type="email" placeholder="example@gmail.com" className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400 focus:dark:border-violet-600" {...register("email", { required: true })} />
+                        {errors.email && <span>Email is required</span>}
+                    </div>
+                    <div className="space-y-2 form-control">
+                        <label htmlFor="password" className="text-sm">Password</label>
+                        <input type="password" placeholder="*******" className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400 focus:dark:border-violet-600" {...register("password", { required: true })} />
+                        {errors.password && <span>Password is required</span>}
+                        <div className="flex justify-end">
+                            <a rel="noopener noreferrer" href="#" className="text-sm hover:underline text-gray-400 dark:text-gray-600">Forgot password?</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="form-control mt-6">
+                    <button type="submit" className="btn btn-primary btn-outline text-lg">Sign in</button>
+                </div>
+            </form>
         </div>
     );
 };
